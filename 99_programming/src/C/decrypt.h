b155efd0de767da5c73a16da618c1a192e5765fa
Thomas Smits <thomsmits@gmail.com>
2018-03-26 22:03:52 +0200

    Deleted week

--- 99_programming/src/C/decrypt.h
+++
@@ -1,6 +0,0 @@
-#ifndef __decrypt_h__
-#define __decrypt_h__
-
-void decrypt(const char* input, char* output);
-
-#endif
--- 99_programming/src/C/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "decrypt.h"
-
-int main() {
-    char input[] = "Qvrfr Nhstnor vfg trfpunssg! Fhcre!";
-    char output[36];
-    decrypt(input, output);
-    printf("%s\n", output);
-}
